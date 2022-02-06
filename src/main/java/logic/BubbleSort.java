ae8732a3d1e11188fa34b6a772557ec153a49d95
Dongkyung Yang <imortaldk@gmail.com>
2022-02-07 04:28:52 +0900

    Design #4 - Sort Interface 인터페이스를 설계하고, 각 구현체가 인터페이스를 구현하는 구조로 수정

--- src/main/java/logic/BubbleSort.java
+++ src/main/java/logic/BubbleSort.java
@@ -6,8 +6,9 @@ import java.util.List;
 // 명세 -
 // 여러가지 자료를 정렬할 수 있으면 좋겠다. 정렬할 수 있는 타입은 서로 비교 가능해야한다.
 // 이를 위해서 Generic type을 사용, 그리고 비교를 위해 존재하는 인터페이스를 사용 Comparable
-public class BubbleSort <T extends Comparable<T>> {
+public class BubbleSort <T extends Comparable<T>> implements Sort<T>{
 
+    @Override
     public List<T> sort(List<T> list) { // 입력리스트를 받아서 출력으로 정렬된 리스트를 내보내준다. 라는 느낌이다.
 
         List<T> output = new ArrayList<>(list);
